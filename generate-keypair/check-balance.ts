a65dfb093d973f43ac34c9f3251f07d11fd47b16
somthn0somthn <lpmaho@gmail.com>
2024-06-22 19:09:12 +0900

    another lesson

---
+++ generate-keypair/check-balance.ts
@@ -0,0 +1,47 @@
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { getDomainKey, getDomainKeySync, NameRegistryState } from '@bonfida/spl-name-service';
+
+const invalidPublicKey = "InvalidPublicKey1234567890";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+const publicKey = keypair.publicKey;
+
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
+function validateSolAddress(address: string): string | null {
+    try {
+        let pubkey = new PublicKey(address)
+        let isSolana = PublicKey.isOnCurve(pubkey.toBuffer())
+        return isSolana ? null : 'Address is not on curve'
+    } catch (error) {
+        return error.message
+    }
+}
+
+async function checkBalance() {
+    const validationError = validateSolAddress(publicKey.toBase58());
+    if (validationError === null) {
+        const balanceInLamports = await connection.getBalance(publicKey);
+        const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+        console.log(
+            `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+        );
+    } else {
+        console.log(`ERROR: the public key ${invalidPublicKey} is invalid. ${validationError}`);
+    }
+}
+
+checkBalance()
+
+const domain = "mccann.sol"
+const connection2 = new Connection(clusterApiUrl("mainnet-beta"));
+const { pubkey } = await getDomainKeySync(domain);
+const realBalanceInLamports = await connection.getBalance(pubkey);
+const realBalanceInSol = realBalanceInLamports / LAMPORTS_PER_SOL ;
+
+console.log(
+    `On mainnet, ${domain} has a balance of ${realBalanceInSol}!`
+);
